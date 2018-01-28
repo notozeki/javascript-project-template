a09f45a199c1b333e04cdeff9a6fbc6fe16008f5
notozeki <notozeki@gmail.com>
2018-01-28 13:20:44 +0900

    Introduce style development environment

--- entry.js
+++ entry.js
@@ -1,1 +1,4 @@
+import './styles/application.scss';
+
 console.log('Hello, world!');
+console.warn('Change the background color as you like :)');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const enableHMR = true;
+const isProduction = process.env.NODE_ENV === 'production';
+const inDevServer = process.argv.find(v => v.includes('webpack-dev-server'));
+const isHMR = inDevServer && enableHMR;
+const extractCSS = !(isHMR) || isProduction;
 
 module.exports = {
   entry: './entry.js',
@@ -18,11 +25,25 @@ module.exports = {
           'babel-loader',
         ],
       },
+      {
+        test: /\.scss$/,
+        use: (extractCSS
+          ? ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: ['css-loader', 'sass-loader'],
+            })
+          : [
+              { loader: 'style-loader', options: { hmr: enableHMR, sourceMap: true } },
+              { loader: 'css-loader', options: { sourceMap: true } },
+              { loader: 'sass-loader', options: { sourceMap: true } },
+            ]
+        )
+      },
     ],
   },
 
   devServer: {
-    hot: true,
+    hot: enableHMR,
     inline: true,
     contentBase: './build',
   },
@@ -33,5 +54,6 @@ module.exports = {
       title: '{title}',
       template: 'index.html',
     }),
+    new ExtractTextPlugin('styles.css'),
   ],
 };
