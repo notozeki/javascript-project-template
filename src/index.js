5295ddb612512a8c4a239b8bc5232c94320347d7
notozeki <notozeki@gmail.com>
2018-07-16 18:53:17 +0900

    Introduce Flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+/* @flow */
 /* eslint-disable no-console */
+
+// $FlowFixMe: Fix "Cannot resolve module" error.
 import './styles/application.scss'
 
-console.log('Hello, world!')
+const message: string = 'Hello, world!'
+
+console.log(message)
 console.warn('Change the background color as you like :)')
